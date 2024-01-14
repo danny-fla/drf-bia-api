ec97aa81f7a648adeecf7fd6a298edf6b78fbb68
danny-fla <dannyfla@live.ie>
2024-01-14 15:01:34 +0000

    add quicksnapdetail view to get, update and delete quicksnaps

--- quicksnap/urls.py
+++ quicksnap/urls.py
@@ -3,4 +3,5 @@ from quicksnap import views
 
 urlpatterns = [
     path('quicksnap/', views.QuicksnapList.as_view()),
+    path('quicksnap/<int:pk>/', views.QuicksnapDetail.as_view())
 ]
--- quicksnap/views.py
+++ quicksnap/views.py
@@ -1,8 +1,10 @@
+from django.http import Http404
 from rest_framework import status, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Quicksnap
 from .serializers import QuicksnapSerializer
+from drf_bia_api.permissions import IsOwnerOrReadOnly
 
 
 class QuicksnapList(APIView):
@@ -29,4 +31,42 @@ class QuicksnapList(APIView):
             )
         return Response(
             serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+class QuicksnapDetail(APIView):
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = QuicksnapSerializer
+
+    def get_object(self, pk):
+        try:
+            quicksnap = Quicksnap.objects.get(pk=pk)
+            self.check_object_permissions(self.request, quicksnap)
+            return quicksnap
+        except Quicksnap.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        quicksnap = self.get_object(pk)
+        serializer = QuicksnapSerializer(
+            quicksnap, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        quicksnap = self.get_object(pk)
+        serializer = QuicksnapSerializer(
+            quicksnap, data=request.data, context={'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request, pk):
+        quicksnap = self.get_object(pk)
+        quicksnap.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
         )
