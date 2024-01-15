4d7459f0a49c4a83ddf7029d239159d141803719
danny-fla <dannyfla@live.ie>
2024-01-15 08:57:49 +0000

    create like serializers. add likelist and likedetail generic views to like and config urls

--- drf_bia_api/urls.py
+++ drf_bia_api/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', include('recipe.urls')),
     path('', include('quicksnap.urls')),
     path('', include('comments.urls')),
+    path('', include('likes.urls')),
 ]
--- likes/models.py
+++ likes/models.py
@@ -6,9 +6,10 @@ from quicksnap.models import Quicksnap
 
 class Like(models.Model):
     """
-    Like model, related to 'owner' and 'post'.
-    'owner' is a User instance and 'post' is a Post instance.
-    'unique_together' makes sure a user can't like the same post twice.
+    Like model, related to 'owner', 'recipe' and 'quicksnap'.
+    'owner' is a User instance, 'recipe' is a 'Recipe' instance and 'quicksnap'
+    is a Quicksnap instance.'unique_together' makes sure a user can't like the
+    same post twice.
     """
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     recipe = models.ForeignKey(
