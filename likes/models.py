8a1f94dd9d821d025a5ec7c3c3f9d91a9700aeaf
danny-fla <dannyfla@live.ie>
2024-01-15 08:40:03 +0000

    create like model

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+from recipe.models import Recipe
+from quicksnap.models import Quicksnap
 
-# Create your models here.
+
+class Like(models.Model):
+    """
+    Like model, related to 'owner' and 'post'.
+    'owner' is a User instance and 'post' is a Post instance.
+    'unique_together' makes sure a user can't like the same post twice.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(
+        Recipe, related_name='likes', on_delete=models.CASCADE
+    )
+    quicksnap = models.ForeignKey(
+        Quicksnap, related_name='likes', on_delete=models.CASCADE
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        unique_together = ['owner', 'recipe', 'quicksnap']
+
+    def __str__(self):
+        return f'{self.owner} {self.recipe} {self.quicksnap}'
