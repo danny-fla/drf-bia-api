239e9279278524b5cd7c7436563295b611655c79
danny-fla <dannyfla@live.ie>
2024-01-18 12:39:05 +0000

    add extra fields to: Profile, Recipe and Quicksnap

--- quicksnap/serializers.py
+++ quicksnap/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Quicksnap
+from QuicksnapLikes.models import QuicksnapLike
 
 
 class QuicksnapSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class QuicksnapSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    quicksnap_like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -25,9 +27,19 @@ class QuicksnapSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_quicksnap_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            quicksnap_like = QuicksnapLike.objects.filter(
+                owner=user, quicksnap=obj
+            ).first()
+            return quicksnap_like.id if quicksnap_like else None
+        return None
+
     class Meta:
         model = Quicksnap
         fields = [
-            'id', 'owner', 'is_owner', 'profile_id', 'profile_image', 'created_at',
-            'updated_at', 'title', 'content', 'image', 'city', 'location'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'created_at','updated_at', 'title', 'content', 'image',
+            'city', 'location','quicksnap_like_id'
         ]
--- recipe/serializers.py
+++ recipe/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Recipe
+from RecipeLike.models import RecipeLike
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    recipe_like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -25,10 +27,19 @@ class RecipeSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_recipe_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            recipe_like = RecipeLike.objects.filter(
+                owner=user, recipe=obj
+            ).first()
+            return recipe_like.id if recipe_like else None
+        return None
+
     class Meta:
         model = Recipe
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',  'created_at',
             'updated_at', 'title', 'ingredients', 'profile_image',
-            'instructions', 'duration', 'image'
+            'instructions', 'duration', 'image', 'recipe_like_id'
         ]
