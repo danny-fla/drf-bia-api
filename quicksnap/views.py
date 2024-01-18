f943223bd5db871504d4398f95eeb8ffd9bdfeda
danny-fla <dannyfla@live.ie>
2024-01-18 11:49:57 +0000

    refactor views.py for: profiles, recipe and quicksnap

--- quicksnap/views.py
+++ quicksnap/views.py
@@ -1,72 +1,26 @@
-from django.http import Http404
-from rest_framework import status, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics, permissions
+from drf_bia_api.permissions import IsOwnerOrReadOnly
 from .models import Quicksnap
 from .serializers import QuicksnapSerializer
-from drf_bia_api.permissions import IsOwnerOrReadOnly
 
 
-class QuicksnapList(APIView):
+class QuicksnapList(generics.ListCreateAPIView):
+    """
+    List quicksnaps or create a quicksnap if logged in
+    The perform_create method associates the quicksnap with the logged in user.
+    """
     serializer_class = QuicksnapSerializer
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly
-    ]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Quicksnap.objects.all()
 
-    def get(self, request):
-        quicksnaps = Quicksnap.objects.all()
-        serializer = QuicksnapSerializer(
-            quicksnaps, many=True, context={'request': request}
-        )
-        return Response(serializer.data)
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
-    def post(self, request):
-        serializer = QuicksnapSerializer(
-            data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save(owner=request.user)
-            return Response(
-                serializer.data, status=status.HTTP_201_CREATED
-            )
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
 
-class QuicksnapDetail(APIView):
+class QuicksnapDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a quicksnap and edit or delete it if you own it.
+    """
+    serializer_class = QuicksnapSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    serializer_class = QuicksnapSerializer
-
-    def get_object(self, pk):
-        try:
-            quicksnap = Quicksnap.objects.get(pk=pk)
-            self.check_object_permissions(self.request, quicksnap)
-            return quicksnap
-        except Quicksnap.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        quicksnap = self.get_object(pk)
-        serializer = QuicksnapSerializer(
-            quicksnap, context={'request': request}
-        )
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        quicksnap = self.get_object(pk)
-        serializer = QuicksnapSerializer(
-            quicksnap, data=request.data, context={'request': request}
-        )
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(
-            serializer.errors, status=status.HTTP_400_BAD_REQUEST
-        )
-
-    def delete(self, request, pk):
-        quicksnap = self.get_object(pk)
-        quicksnap.delete()
-        return Response(
-            status=status.HTTP_204_NO_CONTENT
-        )
+    queryset = Quicksnap.objects.all()
