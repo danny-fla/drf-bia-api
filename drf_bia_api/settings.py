72b27d5b03df0653ad08551dac4c68138573dd9f
danny-fla <dannyfla@live.ie>
2024-01-19 14:23:42 +0000

    add new host

--- drf_bia_api/settings.py
+++ drf_bia_api/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-dannyfla-drfbiaapi-m3o6a01dvio.ws-eu107.gitpod.io', os.environ.get('ALLOWED_HOST')]
+ALLOWED_HOSTS = ['8000-dannyfla-drfbiaapi-m3o6a01dvio.ws-eu107.gitpod.io', 'drf-bia-api-8cf791bcdd63.herokuapp.com']
 
 
 # Application definition
