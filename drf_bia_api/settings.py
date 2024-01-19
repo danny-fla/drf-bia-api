77fc405a8f839741d53ecc50c2e859932b6dc542
danny-fla <dannyfla@live.ie>
2024-01-19 11:06:57 +0000

    remove cloudinary from settings.py

--- drf_bia_api/settings.py
+++ drf_bia_api/settings.py
@@ -17,10 +17,6 @@ if os.path.exists('env.py'):
     import env
 
 
-CLOUDINARY_STORAGE = {
-    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL'),
-}
-
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
