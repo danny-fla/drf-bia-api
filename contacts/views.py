f158768be0b65a3376151bd03d0a3286c3e2facf
danny-fla <dannyfla@live.ie>
2024-02-02 14:24:52 +0000

    contacts: create contactlist and contactdetail views using generic configure urls.py

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from .models import Contact
+from .serializers import ContactSerializer
 
-# Create your views here.
+
+class ContactList(generics.ListCreateAPIView):
+    """
+    List contacts or create a contact if logged in.
+    """
+    serializer_class = ContactSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Contact.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class ContactDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve a contact, or update or delete it by id.
+    """
+    permission_classes = [permissions.IsAdminUser]
+    serializer_class = ContactSerializer
+    queryset = Contact.objects.all()
